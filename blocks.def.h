a3a8eb13d87d326b9940e74e99082582518e4aa0
Filip Rojek <filip@filiprojek.cz>
2023-02-24 10:48:23 +0100

    scripts added

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	  /*Command*/		                   /*Update Interval*/	/*Update Signal*/
-    { " NET ",   "/opt/dwmblocks_scripts/dwm_ipaddr",           30,  0 },
+    { " SSID ",  "/opt/dwmblocks_scripts/dwm_ssid",             1,   0 },
+    { "NET ",    "/opt/dwmblocks_scripts/dwm_ipaddr",           1,   0 },
     { "MEM ",    "/opt/dwmblocks_scripts/dwm_memory",           30,  0 },
     { "",        "/opt/dwmblocks_scripts/dwm_volume withicon",  1,   10 },
     { "",        "/opt/dwmblocks_scripts/dwm_battery withicon", 1,   10 },
